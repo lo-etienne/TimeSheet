0f83e4e4f6ff3c36dc229a08d73395d6889d8fb9
Maxime PRODHOMME <m.prodhomme@student.helmo.be>
2021-03-24 21:14:06 +0100

    Création de la page de formulaire de connexion

--- app/src/main/java/com/example/timesheet/MainActivity.java
+++ app/src/main/java/com/example/timesheet/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.timesheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "onCreate called");
         setContentView(R.layout.activity_main);
     }
 }
