04559512b030de3cf5f3177faedc0f563c28eac6
e180908 <g2tyYVxM>
2021-03-24 15:37:28 +0100

    WIP : implémentation de la classe abstraite Day, et des classes Holiday et WorkDay qui extends Day, il faudra ajouter les annotations une fois que la DB sera implémentée

---
+++ app/src/main/java/com/example/timesheet/model/WorkDay.java
@@ -0,0 +1,77 @@
+package com.example.timesheet.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class WorkDay extends Day {
+
+    private String workingHours;
+    private String notWorkingHours;
+    private String status;
+
+    public WorkDay(final UUID uuid,
+                   final String name,
+                   final Date date,
+                   final String workingHours,
+                   final String notWorkingHours,
+                   final String status) {
+        super(uuid, name, date);
+        this.workingHours = workingHours;
+        this.notWorkingHours = notWorkingHours;
+        this.status = status;
+    }
+
+    @Override
+    public UUID getUuid() {
+        return super.getUuid();
+    }
+
+    @Override
+    public void setUuid(final UUID uuid) {
+        super.setUuid(uuid);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(final String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public Date getDate() {
+        return super.getDate();
+    }
+
+    @Override
+    public void setDate(final Date date) {
+        super.setDate(date);
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(final String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public String getNotWorkingHours() {
+        return notWorkingHours;
+    }
+
+    public void setNotWorkingHours(final String notWorkingHours) {
+        this.notWorkingHours = notWorkingHours;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
+    }
+}
